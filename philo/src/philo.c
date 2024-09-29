c470f8574388ae5894b9adfa20223f554eb09a58
lui0403 <lumanpower2004@gmail.com>
2024-09-29 19:48:43 +0900

    fixed error hundling

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: luebina <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 18:21:50 by luebina           #+#    #+#             */
-/*   Updated: 2024/09/26 19:29:24 by luebina          ###   ########.fr       */
+/*   Updated: 2024/09/29 19:46:06 by luebina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	main(int argc, char **argv)
 {
 	t_table	table;
 
-	(void)argc;
+	if (argc != 5 && argc != 6)
+		return (0);
 	parse_input(&table, argv);
 	if (table.philo_num < 0 || table.time_to_die < 0
 		|| table.time_to_eat < 0 || table.time_to_sleep < 0
